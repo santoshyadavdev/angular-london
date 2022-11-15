6a5ea42b6087824cc656bd80dbbc1be7f8ebf35b
Santosh Yadav <santosh.yadav198613@gmail.com>
2022-11-15 20:23:25 +0000

    added libs

--- apps/angular-demo-app/src/app/app.module.ts
+++ apps/angular-demo-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StateModule } from '@angular-london/state';
+import { UiModule } from '@angular-london/ui';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, UiModule, StateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
